334c01d459062cbe76fc91821f3fc5e326d66fdc
Andrii <mail.vav@gmail.com>
2022-07-16 17:17:10 +0300

    Edit ShopController

--- controllers/ShopController.js
+++ controllers/ShopController.js
@@ -2,9 +2,8 @@ import ShopModel from '../models/Shop.js';
 
 export const getAll = async (req, res) => {
     try {
-        // const shops = await ShopModel.find().exec();
-        // res.json(shops);
-        res.json('shops');
+        const shops = await ShopModel.find().exec();
+        res.json(shops);
     } catch (err) {
         console.log(err);
         return res.status(500).json({
